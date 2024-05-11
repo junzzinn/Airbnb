3da3e880fc84dd2fd6f51f78699c038b182c367e
daniel furukawa <danifuruka@gmail.com>
2024-05-11 12:40:46 -0300

    duplicated tables deleted

--- db/migrate/20240511132441_create_chats.rb
+++
@@ -1,9 +0,0 @@
-class CreateChats < ActiveRecord::Migration[7.1]
-  def change
-    create_table :chats do |t|
-      t.references :booking, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
