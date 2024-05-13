661fe3edd225f40f8bf926ff187730060900d12f
Andre Neves <andren.p@hotmail.com>
2024-05-13 20:01:35 -0300

    não consegui centralizar essa desgraça 😭😭😭

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,12 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-import CategoryController from "../controllers/category_controller";
-
-const application = Application.start();
-const context = require.context("../controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
-
-application.register("category", CategoryController);
