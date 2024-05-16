2528d780397e255153259d112a171dbb334ed004
renanv <renanvtt@hotmail.com>
2024-05-16 20:37:08 -0300

    Search bar done with filter and footer

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,4 +1,11 @@
 class Residence < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_address,
+   against: [ :title, :address],
+   using: {
+    tsearch: { prefix: true }
+   }
 end
